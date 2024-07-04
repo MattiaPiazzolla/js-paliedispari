17bff4ec2196a995e2aee2945526278ff513d958
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-04 19:27:56 +0200

    [UPD] created a function to generate a random number

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -1,2 +1,6 @@
 // RECUPERO IL PULSANTE DI AVVIO 
 const startButton = document.getElementById('startBtn');
+// CREO UNA FUNZIONE PER GENERARE UN NUMERO RANDOM
+function randomNumber() {
+    return Math.floor(Math.random() * 5) + 1;
+}
